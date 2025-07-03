780767a08ab17d872f0af95887033a57e57dabe1
DipalokSen <aksharsen68@gmail.com>
2025-07-03 11:10:42 +0600

    Implemented File Card

--- components/FileCard.tsx
+++ components/FileCard.tsx
@@ -2,15 +2,28 @@ import Link from 'next/link'
 import { Models } from 'node-appwrite'
 import React from 'react'
 import Thumbnail from "@/components/Thumbnail"
+import { convertFileSize } from '@/lib/utils'
+import FormattedDateTime from './FormattedDateTime'
 
 
 const FileCard = ({file}:{file:Models.Document}) => {
   return (
-    <Link href={file.url} target='"_blank'>
+    <Link href={file.url} target='"_blank' className='file-card'>
       <div className="flex justify-between">
-        <Thumbnail type={file.type1} extension={file.extension} url={file.url} />   
+        <Thumbnail type={file.type1} extension={file.extension} url={file.url} />
+
+            <div className="flex flex-col items-end justify-between">
+              Action Goto...
+             <p>   {convertFileSize(file.size)}</p>
+            </div>
+
         </div>
-        {file.name}
+        <div className="file-card-details">
+          <p className="subtitle-2 line-clamp-1">{file.name}</p>
+          <FormattedDateTime date={file.$createdAt} className="body-2 text-light-200"/>
+
+         <p className="caption text-light-200 line-clamp-1">By: {file.owner.fullName}</p>
+        </div>
     </Link >
   )
 }
